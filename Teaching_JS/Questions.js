8735f18aa104ae832b549b1d78416179ba1754f1
Sabonchi <hsadeveloper@gmail.com>
2021-09-25 22:21:27 -0700

    Update Questions.js

--- Teaching_JS/Questions.js
+++ Teaching_JS/Questions.js
@@ -1,37 +1,18 @@
 // Write a function to find the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
-//Input: strs = ["flower","flow","flight"]
-//Output: "fl"
-//Input: strs = ["dog","racecar","car"]
-//Output: ""
-//Explanation: There is no common prefix among the input strings.
-
+//Input: haystack = "hello", needle = "ll"
+//Output: 2
+// Input: haystack = "aaaaa", needle = "bba"
+// Output: -1
 //Sol
 
+
 /**
- * @param {string[]} strs
- * @return {string}
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
  */
-var longestCommonPrefix = function(strs) {
-let prefix=""
-
- 
+var strStr = function(haystack, needle) {
     
-for (var i=0;i<strs[0].length;i++){
-     let prefixtemp=strs[0].charAt(i);
-    
-     // if (strs.length ==1)
-     //      return strs[0]
-    
-    
-    for (var j=1;j<strs.length;j++){
-         if(strs[j].charAt(i)!==prefixtemp){
-           return prefix
-         }  
-     }
-
-       prefix+=""+strs[0].charAt(i)
-    
-}
-return prefix
+    return haystack.indexOf(needle)
     
 };
